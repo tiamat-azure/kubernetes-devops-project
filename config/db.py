aadfa4c532a02070bf95430126faaa1aec2156bf
tiamat-azure <48587107+tiamat-azure@users.noreply.github.com>
2023-10-26 01:16:34 +0000

    feat: variabiliser les parametres de connexion DB

--- config/db.py
+++ config/db.py
@@ -1,7 +1,19 @@
+import os
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("postgresql://admin:password@db:5432/storedb")
 
+# Récupérer les valeurs via les variables d'environnement
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PWD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+# Créez la chaîne de connexion en utilisant les variables
+db_connection_string = f"postgresql://{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Créez l'objet engine en utilisant la chaîne de connexion
+engine = create_engine(db_connection_string)
 meta = MetaData()
 
 conn = engine.connect()
